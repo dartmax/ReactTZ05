370e5452f3cb74bca437107d588c1ff8f8d715a4
dartmax <promostend@gmail.com>
2021-03-03 23:56:59 +0200

    Updates

---
+++ precache-manifest.f00e65bf38638a132b05e503f3b1c348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4249cf454eea11e6cc728e608b423c9",
+    "url": "/ReactTZ05/index.html"
+  },
+  {
+    "revision": "ee177f1b015fce3d6045",
+    "url": "/ReactTZ05/static/css/main.1cdefe0b.chunk.css"
+  },
+  {
+    "revision": "fb93d062f81a574ad723",
+    "url": "/ReactTZ05/static/js/2.5898860c.chunk.js"
+  },
+  {
+    "revision": "35fc78335d3889e75d35aa495bc525ba",
+    "url": "/ReactTZ05/static/js/2.5898860c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee177f1b015fce3d6045",
+    "url": "/ReactTZ05/static/js/main.6aefc34a.chunk.js"
+  },
+  {
+    "revision": "dc9233f84eae8ff487a7",
+    "url": "/ReactTZ05/static/js/runtime-main.629301c2.js"
+  }
+]);
